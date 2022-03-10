d3e19be2e70bc3544a8f7004472a06adc0a0d0fe
Temilope Owolabi <temilope.owolabi@ie.abb.com>
2022-03-10 09:55:00 +0000

    Set up Cypress E2E tests

---
+++ cypress/integration/smoke.spec.ts
@@ -0,0 +1,8 @@
+/// <reference types="cypress" />
+
+describe('Smoke Test', () => {
+    it('can view the home page', () => {
+      cy.visit('/');
+      cy.contains('Learn React');
+    });
+  });
